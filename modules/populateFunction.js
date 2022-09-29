26ab687ecfa029faf2632ec87b86f2c55888378c
Nahnah Ahjuwah <nahnahahjuwah@gmail.com>
2022-09-29 05:21:26 -0700

    Refactor populate function to include the completed[boo]

--- modules/populateFunction.js
+++ modules/populateFunction.js
@@ -5,10 +5,11 @@ const holder = document.getElementById('list-holder');
 const populate = () => {
   tasks.forEach((element) => {
     const add = document.createElement('div');
-    add.dataset.elementid = element.id;
+    add.dataset.elementid = element.index;
     add.innerHTML = `
   <div class="container">
-  <span><input type='checkbox'>${element.string}</span>
+  <span><input type='checkbox'>${element.description}</span>
+  <div><p>${element.completed ? 'completed' : 'not completed'}</p></div>
   <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
   </div><hr>
   `;
