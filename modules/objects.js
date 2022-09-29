badce79eb71dc493192c05a19c9c49512909b303
Nahnah Ahjuwah <nahnahahjuwah@gmail.com>
2022-09-29 05:18:49 -0700

    Make changes to the array objects to suit review suggestions

--- modules/objects.js
+++ modules/objects.js
@@ -1,23 +1,23 @@
 const tasks = [
   {
-    string: 'Complete to do list project',
-    boo1: 'Join stand-up Call',
-    id: '1',
+    description: 'Complete to do list project',
+    completed: false,
+    index: '1',
   },
   {
-    string: 'Join stand-up Call',
-    boo1: 'Not completed',
-    id: '2',
+    description: 'Join in stand-up Call later',
+    completed: false,
+    index: '2',
   },
   {
-    string: 'Arange a call',
-    boo1: 'Join stand-up Call',
-    id: '3',
+    description: 'Arange a call with partner',
+    completed: false,
+    index: '3',
   },
   {
-    string: 'Control the teams project',
-    boo1: 'Join stand-up Call',
-    id: '4',
+    description: 'Control the teams project ',
+    completed: false,
+    index: '4',
   },
 ];
 export default tasks;
